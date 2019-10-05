7357d9d6fc1af40b5a0d7731d94d450033f37a07
Ruslan Tugov <ruslan.tugov@gmail.com>
2019-10-05 11:43:25 -0400

    updating my brach to pull from master

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,22 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+ 
+  // PUT route for updating todos. We can get the updated todo data from req.body
+  app.put("/api/bridge", function (req, res) {
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    // Update takes in an object describing the properties we want to update, and
+    // we use where to describe which objects we want to update
+    db.Bridge.update({
+      completed: true
+    }, {
+      where: {
+        userID: req.body.userID,
+        activityID: req.body.activityID
+      }
+    }).then(function (dbBridge) {
+      res.json(dbBridge);
     });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+      
   });
 };
